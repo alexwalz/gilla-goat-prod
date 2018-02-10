7407b517e689364b5d6876a9a48d6becd3bb9cb4
alexwalz <alexwalz@icloud.com>
2018-02-10 11:01:23 -0700

    I give up

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-app.set( 'PORT', ( process.env.PORT || 5000 ));
+const PORT = process.env.PORT || 5000 
 var bodyParser = require("body-parser");
 var app = express();
 var db = require("./models");
