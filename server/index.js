54cf9eef02fa4c044554397ff8777b1b6ae62849
alexwalz <alexwalz@icloud.com>
2018-02-10 00:52:17 -0700

    heroku test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-const PORT = process.env.PORT || 5002;
+app.set( 'PORT', ( process.env.PORT || 5000 ));
 var bodyParser = require("body-parser");
 var app = express();
 var db = require("./models");
@@ -54,10 +54,14 @@ if (cluster.isMaster) {
 } else {
   db.sequelize.sync({ force: false }).then(function() {
     console.log("\n\nDatabase Tables are ready 👌")
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT +" 👂👂\n");
-      // console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-    });
+    // app.listen(PORT, function() {
+    //   console.log("App listening on PORT " + PORT +" 👂👂\n");
+    //   // console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+    // });
+
+    app.listen( app.get( 'PORT' ), function() {
+      console.log( 'Node server is running on port ' + app.get( 'port' ));
+      });
   });
 
 }
